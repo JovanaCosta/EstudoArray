9bc76bb8006baafb527440dbbaa82dda24a8dc21
JovanaCosta <jovanasouzacosta76@gmail.com>
2025-07-12 13:32:43 -0300

    ArrayService Primeira Parte

---
+++ repos/EstudoArray/EstudoArray/ArrayService.cs
@@ -0,0 +1,46 @@
+﻿namespace EstudoArray
+{
+    public class ArrayService
+    {
+        public void Execute()
+        {
+            var meuArray = new int[15];
+            meuArray[0] = 22;
+            meuArray[1] = 27;
+            meuArray[2] = 33;
+            meuArray[3] = 18;
+            meuArray[4] = 9;
+            meuArray[5] = 15;
+            meuArray[6] = 44;
+            meuArray[7] = 54;
+            meuArray[8] = 21;
+            meuArray[9] = 13;
+            meuArray[10] = 78;
+            meuArray[11] = 12;
+            meuArray[12] = 96;
+            meuArray[13] = 61;
+            meuArray[14] = 55;
+
+
+            var nomesArray = new string[10];
+            nomesArray[0] = "Jovana";
+            nomesArray[1] = "William";
+            nomesArray[2] = "Bille";
+            nomesArray[3] = "Belina";
+            nomesArray[4] = "Ione";
+            nomesArray[5] = "Iolanda";
+            nomesArray[6] = "Luiz";
+            nomesArray[7] = "Eliseu";
+            nomesArray[8] = "Nina";
+            nomesArray[9] = "Lili";
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (nomesArray[i] == "Belina")
+                {
+                    nomesArray[i] = "";
+                }
+            }
+        }
+    }
+}
---
+++ repos/EstudoArray/EstudoArray/ClassExemple.cs
@@ -0,0 +1,61 @@
+﻿namespace EstudoArray
+{
+    public class ClassExemple
+    {
+        // Atributos da classe
+
+        public int Atributo1 { get; set; }
+        public string Atributo2 { get; set; }
+
+
+        // Contrutor
+        public ClassExemple(int atributo1, string atributo2)
+        {
+            Atributo1 = atributo1;
+            Atributo2 = atributo2;
+        }
+
+
+        // Metodo sem retorno (void)
+        public void NomeMetodo()
+        {
+
+        }
+
+        // Metodo com retorno
+        public int NomeMetodo2()
+        {
+            return 1;
+        }
+
+        // Metodo com parametros de entrada
+        public void NomeMetodo3(int parametro1, string parametro2)
+        {
+
+        }
+
+
+        // ******* Exemplos Gerais *******
+
+        public void Array()
+        {
+            //Criando array com 10 posicoes
+            var meuArray = new int[10];
+
+            //Adicionando valores no array
+            meuArray[0] = 22;
+            meuArray[1] = 17;
+            meuArray[2] = 13;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (meuArray[i] == 22)
+                {
+                    meuArray[i] = 0;
+                    //meuArray[i] = "";
+                }
+
+            }
+        }
+    }
+}
